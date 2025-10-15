66b93ec2de8ff543f8fc3a4db1da9b1aef0e7cc0
Justin Forest <hex@umonkey.net>
2025-10-15 17:01:52 +0400

    Add an Intro block

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
     import DiversityChart from '$lib/components/charts/metrics/DiversityChart.svelte';
     import AgeChart from '$lib/components/charts/metrics/AgeChart.svelte';
     import MainBlock from '$lib/components/blocks/MainBlock.svelte';
+    import IntroBlock from '$lib/components/blocks/IntroBlock.svelte';
 </script>
 
 <svelte:head>
@@ -16,10 +17,7 @@
 </svelte:head>
 
 <MainBlock />
-
-<Narrow>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</Narrow>
+<IntroBlock />
 
 <Narrow>
     <ChartGrid>
